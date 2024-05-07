3b5110efc5b5eb76ffd5f92d86fc74019abcfc5e
Colin Swaney <colinswaney@princeton.edu>
2024-05-07 09:00:03 -0400

    Update speaker_change_detection_pyannote.py

--- src/audiotextspeakerchangedetect/SpeakerChangeDetection/PyAnnote/speaker_change_detection_pyannote.py
+++ src/audiotextspeakerchangedetect/SpeakerChangeDetection/PyAnnote/speaker_change_detection_pyannote.py
@@ -3,8 +3,16 @@ from pyannote.audio import Pipeline
 import torch
 from typing import Union
 
-def pyannote_speakerchangedetection(audio_file_input_path, audio_file_input_name, min_speakers, max_speakers, hf_access_token,
-                                 device: Union[str, torch.device]=None, pyannote_model_path=None):
+
+def pyannote_speakerchangedetection(
+    audio_file: str,
+    min_speakers: int,
+    max_speakers: int,
+    hf_access_token: str,
+    device: Union[str, torch.device] = None,
+    pyannote_model_path: str=None,
+):
+    """..."""
 
     if device is None:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -12,21 +20,25 @@ def pyannote_speakerchangedetection(audio_file_input_path, audio_file_input_name
         device = torch.device(device)
 
     if pyannote_model_path:
-        detection_pipeline = Pipeline.from_pretrained(os.path.join(pyannote_model_path, 'config.yaml'))
+        print(f"Using Pyannote model: {pyannote_model_path}")
+        detection_pipeline = Pipeline.from_pretrained(
+            os.path.join(pyannote_model_path, "config.yaml")
+        )
     else:
-        detection_pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", use_auth_token=hf_access_token)
+        print("Using default Pyannote model: pyannote/speaker-diarization-3.1")
+        detection_pipeline = Pipeline.from_pretrained(
+            "pyannote/speaker-diarization-3.1", use_auth_token=hf_access_token
+        )
 
     detection_pipeline.to(device)
+    detection_result = detection_pipeline(
+        audio_file,
+        min_speakers=min_speakers,
+        max_speakers=max_speakers,
+    )
 
-    detection_result = detection_pipeline(os.path.join(audio_file_input_path, audio_file_input_name),
-                                                min_speakers = min_speakers, max_speakers = max_speakers)
-
-    timestamp_speaker = {}
+    labeled_timestamps = {}
     for turn, _, speaker in detection_result.itertracks(yield_label=True):
-        timestamp_speaker[turn.start] = speaker
+        labeled_timestamps[turn.start] = speaker
 
-    return timestamp_speaker
-
-
-
-
+    return labeled_timestamps
