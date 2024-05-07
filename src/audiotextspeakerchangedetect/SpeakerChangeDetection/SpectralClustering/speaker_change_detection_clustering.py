a550087bafe0971cf484d0574ec0d967b3325a11
Colin Swaney <colinswaney@princeton.edu>
2024-05-07 09:00:39 -0400

    Update speaker_change_detection_clustering.py

--- src/audiotextspeakerchangedetect/SpeakerChangeDetection/SpectralClustering/speaker_change_detection_clustering.py
+++ src/audiotextspeakerchangedetect/SpeakerChangeDetection/SpectralClustering/speaker_change_detection_clustering.py
@@ -4,36 +4,35 @@ import os
 from resemblyzer import preprocess_wav, VoiceEncoder, sampling_rate
 from spectralcluster import SpectralClusterer
 
-# Create Dict to Map Time Segment to Speaker
+
 def create_labelling(labels, wav_splits):
+    """Create a dict to map time segments to speakers."""
     times = [((s.start + s.stop) / 2) / sampling_rate for s in wav_splits]
     start_time = 0
-
     timestamp_speaker = {}
-
     for i, time in enumerate(times):
         if i > 0 and labels[i] != labels[i - 1]:
-            timestamp_speaker[start_time] = 'speaker' + str(labels[i - 1])
+            timestamp_speaker[start_time] = "speaker" + str(labels[i - 1])
             start_time = time
         if i == len(times) - 1:
-            timestamp_speaker[start_time] = 'speaker' + str(labels[i])
+            timestamp_speaker[start_time] = "speaker" + str(labels[i])
     return timestamp_speaker
 
-def spectralclustering_speakerchangedetection(audio_file_input_path, audio_file_input_name,
-                                           min_speakers, max_speakers, device: Union[str, torch.device]='gpu'):
-    wav = preprocess_wav(os.path.join(audio_file_input_path, audio_file_input_name))
 
+def spectralclustering_speakerchangedetection(
+    audio_file,
+    min_speakers,
+    max_speakers,
+    device: Union[str, torch.device] = "gpu",
+):
+    """..."""
+
+    wav = preprocess_wav(audio_file)
     encoder = VoiceEncoder(device)
-    _, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, rate=16)
-
-    # Clustering of Speakers based on Embedding
-    clusterer = SpectralClusterer(
-        min_clusters=min_speakers,
-        max_clusters=max_speakers)
-
+    _, cont_embeds, wav_splits = encoder.embed_utterance(
+        wav, return_partials=True, rate=16
+    )
+    clusterer = SpectralClusterer(min_clusters=min_speakers, max_clusters=max_speakers)
     labels = clusterer.predict(cont_embeds)
-    timestamp_speaker = create_labelling(labels, wav_splits)
-    return timestamp_speaker
-
-
-
+    timestamp_speakers = create_labelling(labels, wav_splits)
+    return timestamp_speakers
