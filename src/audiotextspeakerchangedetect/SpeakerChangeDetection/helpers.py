ee75483332b3c93b8bb3e42df2fd9e4f269f4dcd
Colin Swaney <colinswaney@princeton.edu>
2024-05-07 08:58:51 -0400

    Update helpers.py

--- src/audiotextspeakerchangedetect/SpeakerChangeDetection/helpers.py
+++ src/audiotextspeakerchangedetect/SpeakerChangeDetection/helpers.py
@@ -1,29 +1,35 @@
 from bisect import bisect_left
 
-def merge_detection_audio_results_with_transcription(timestamp_speaker, whisper_df):
-    ''''
-    Append speaker change detection results with transcription results
-    '''
 
-    timestamp_list= list(timestamp_speaker.keys())
+def merge_detection_audio_results_with_transcription(
+    # TODO: add type hints
+    timestamp_speaker,
+    whisper_df,
+):
+    """Append speaker change detection results with transcription results.
 
-    speakers= [None] * whisper_df.shape[0]
-    speaker_change = [None] * whisper_df.shape[0]
+    Args:
+        timestamp_speaker: ...
+        whisper_df: ...
 
+    Returns:
+        speakers: ...
+        speaker_changes: ...
+    """
+
+    timestamps = list(timestamp_speaker.keys())
+    speakers = [None] * whisper_df.shape[0]
+    speaker_changes = [None] * whisper_df.shape[0]
     prev_speaker, curr_speaker = None, None
-
     for idx, segment in whisper_df.iterrows():
-        # account for lack of zero timestamp
-        timestamp_idx = max(bisect_left(timestamp_list, segment['start']) - 1, 0)
-
-        curr_speaker = timestamp_speaker[timestamp_list[timestamp_idx]]
-
+        timestamp_idx = max(
+            bisect_left(timestamps, segment["start"]) - 1, 0
+        )  # adjust for missing zero timestamp
+        curr_speaker = timestamp_speaker[timestamps[timestamp_idx]]
         speakers[idx] = curr_speaker
-
         if curr_speaker != prev_speaker:
-            speaker_change[idx] = True
+            speaker_changes[idx] = True
         else:
-            speaker_change[idx] = False
-
+            speaker_changes[idx] = False
         prev_speaker = curr_speaker
-    return speakers, speaker_change
+    return speakers, speaker_changes
